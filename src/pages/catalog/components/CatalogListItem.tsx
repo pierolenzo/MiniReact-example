ff8c3caf5a0d7b47902be32fc95b154844d55544
pierolenzo <pierolenzo@gmail.com>
2021-11-19 01:07:37 +0100

    Feat: add method openMap

--- src/pages/catalog/components/CatalogListItem.tsx
+++ src/pages/catalog/components/CatalogListItem.tsx
@@ -7,9 +7,14 @@ interface CatalogListItemProps {
 }
 
 export const CatalogListItem:React.FC<CatalogListItemProps> = ({ item }) => {
+
+    const openMap = () => {
+        window.open(`https://www.google.com/maps/place/${item.city}`);
+    }
+    
     return (
         <div className="col-sm-12">
-          <MapQuest city={item.city} />
+          <MapQuest city={item.city} onClick={openMap}/>
           <h5>{item.title}</h5>
           <p>{item.address} - {item.city}</p>
         </div>
